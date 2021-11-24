ff33a8e7ee73184f60f460efa73aa5fd1584d2eb
Frank Liu <fliu@redcanari.com>
2021-11-23 23:29:28 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<template lang='pug'>
+MainPage
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
 
-@Options({
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import MainPage from './pages/MainPage.vue';
+
+export default defineComponent({
   components: {
-    HelloWorld,
+    MainPage,
   },
 })
-export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
