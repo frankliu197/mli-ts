8df2a4161c247262467f2d2ef4245893950b50b1
frankliu197 <frankliu197@gmail.com>
2021-11-23 23:29:28 -0500

    first commit

---
+++ src/components/InputBox.vue
@@ -0,0 +1,44 @@
+<template lang='pug'>
+
+textarea(ref="textarea" @keyup.page-down="insertSuggestDropdown")
+
+.absolute-dropdown
+  n-dropdown(:show="showDropdown")
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'InputBox',
+  methods: {
+    insertSuggestDropdown(){
+      this.showDropdown = true
+    }
+  },
+  data: function(){
+    return {
+      showDropdown: false
+    }
+  },
+  computed: {
+   /* dropdownLocation: function(){
+      const textarea = this.$refs.textarea
+      const {x, y} = textarea.getBoundingClientRect()
+      let pos = textarea.selectionStart ?? 0
+      return { x: x + pos, y: y}
+    }*/
+  }
+})
+</script>
+
+<style lang='scss' scoped>
+textarea {
+  width: 90%;
+  margin: 15px;  
+}
+
+.absolute-dropdown {
+  position: absolute;
+}
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,16 @@
+<template lang='pug'>
+InputBox 
+</template>
+
+<script>
+import InputBox from "../components/InputBox.vue"
+export default {
+  name: 'MainPage',
+  components: {
+    InputBox
+  }
+}
+</script>
+
+<style>
+</style>
