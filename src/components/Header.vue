178161f0ba4595c1ec33e92a632179ef59ab540c
aeiou80 <cameronjdavis@cmail.carleton.ca>
2021-11-25 15:42:47 -0500

    WIP: implementing light/dark mode (trying to get the checkbox to actually toggle it)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,48 @@
 <template lang="">
-<div class="nav">
+<header>
     <n-button class="nav-btn" color="#66ff33" strong secondary round type="info">Home</n-button>
     <n-button class="nav-btn" color="#009900" ghost>Favourites</n-button>
     <n-button class="nav-btn" color="#009900" ghost>Customize</n-button>
-</div>
+    <span> 
+        <input 
+            type="checkbox" 
+            :checked="(mode==='dark')?'checked':false"
+        /> {{ mode }} mode
+    </span>
+</header>
 </template>
+
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-    name:'Header'
+    name:'Header',
+    props: ['mode']
 })
 </script>
+
 <style lang="scss">
-.nav {
-    background-color: rgb(58, 58, 58);
+header {
+    background: rgb(58, 58, 58);
     overflow: hidden;
     text-align: center;
-    padding: 10px;
+    padding: 15px;
+    // transition: background 0.3s ease-in-out;
+}
+.dark header {
+  background: #121c25;
+  color: rgb(183, 189, 183);
+}
+header {
+    background: #cccccc;
+    color: rgb(63, 65, 63);
+}
+.dark .nav-btn {
+    color: rgb(183, 189, 183);
+    margin: 0 10px 0 10px;
 }
 .nav-btn {
+    color: rgb(63, 65, 63);
     margin: 0 10px 0 10px;
 }
 </style>
