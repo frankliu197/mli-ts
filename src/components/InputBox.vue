bb546c20f3626fd13eb2c441a605a855b66017df
Frank Liu <fliu@redcanari.com>
2021-11-24 20:56:01 -0500

    Added moving dropdown

--- src/components/InputBox.vue
+++ src/components/InputBox.vue
@@ -2,32 +2,41 @@
 
 textarea(ref="textarea" @keyup.page-down="insertSuggestDropdown")
 
-.absolute-dropdown
-  n-dropdown(:show="showDropdown")
+n-dropdown
+  .absolute-dropdown(v-show="showDropdown" :style="coords") adfsd
 </template>
 
-<script>
+<script lang='ts'>
 import { defineComponent } from 'vue'
+interface Coordinates { left: string, top: string, height: string, width: string}
 
+let MIN_WIDTH = 50;
 export default defineComponent({
   name: 'InputBox',
   methods: {
     insertSuggestDropdown(){
-      this.showDropdown = true
+      this.showDropdown = !this.showDropdown
     }
   },
   data: function(){
     return {
-      showDropdown: false
+      showDropdown: false,
+      coords: {} as Coordinates
     }
   },
-  computed: {
-   /* dropdownLocation: function(){
-      const textarea = this.$refs.textarea
-      const {x, y} = textarea.getBoundingClientRect()
-      let pos = textarea.selectionStart ?? 0
-      return { x: x + pos, y: y}
-    }*/
+  watch: {
+    showDropdown: function(val) {
+      if (!val) {
+        return 
+      }
+      const textarea : HTMLTextAreaElement = this.$refs.textarea as HTMLTextAreaElement
+      let fontsize = {} as any;
+      fontsize.width = textarea.clientWidth + 1
+      fontsize.height = textarea.clientHeight + 1
+      let {x, y} = textarea.getBoundingClientRect()
+      const offset = textarea.selectionStart * 12 ?? 0
+      this.coords = {left: `calc(${x}px + ${offset}px)`, top: y + "px", width: MIN_WIDTH + "px", height: 100 + "px"}
+    }
   }
 })
 </script>
@@ -36,9 +45,11 @@ export default defineComponent({
 textarea {
   width: 90%;
   margin: 15px;  
+  font-size: 20px;
 }
 
 .absolute-dropdown {
   position: absolute;
+  background-color: yellow;
 }
 </style>
