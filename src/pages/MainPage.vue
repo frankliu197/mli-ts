ca3a6ab2652cf591a86b1b205287025c1856dca2
aeiou80 <cameronjdavis@cmail.carleton.ca>
2021-11-25 15:42:47 -0500

    WIP: implementing light/dark mode (trying to get the checkbox to actually toggle it)

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,15 +1,12 @@
 <template lang='pug'>
-Header
 InputBox
 </template>
 
 <script lang="ts">
 import InputBox from "../components/InputBox.vue"
-import Header from "../components/Header.vue"
 export default {
   name: 'MainPage',
   components: {
-    Header,
     InputBox
   }
 }
