30fc5ca2b36330cdfd6846b0133df1223b89f850
aeiou80 <cameronjdavis@cmail.carleton.ca>
2022-02-11 16:03:12 -0500

    added set origin name to symbols.json

--- scripts/ParseSymbolFile.py
+++ scripts/ParseSymbolFile.py
@@ -9,8 +9,10 @@ with open(sys.argv[1], encoding="utf8") as f:
     for line in f:
         data = line.replace("-", "_")
         data = data.split()
+        if data[0] == "setName":
+            setName = data[1].replace('_', ' ')
         if data[0] != "setName" and len(data) == 4:
-            dataset.append({"unicode": int(data[1]), "name": data[2].replace('_', ' '), "symbol": data[3], "keywords": {}, "symbols": []})
+            dataset.append({"set": setName, "unicode": int(data[1]), "name": data[2].replace('_', ' '), "symbol": data[3], "keywords": {}, "symbols": []})
 
 with open(sys.argv[1].rsplit('.', 1)[0] + '.json', 'w') as f:
     json.dump(dataset, f, indent=4)
