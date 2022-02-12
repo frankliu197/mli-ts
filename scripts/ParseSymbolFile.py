2ba369e8a6567d15392315071f1afb0ca9b29fe3
aeiou80 <cameronjdavis@cmail.carleton.ca>
2022-02-12 14:25:10 -0500

    symbol name strings now lower case

--- scripts/ParseSymbolFile.py
+++ scripts/ParseSymbolFile.py
@@ -10,7 +10,7 @@ with open(sys.argv[1], encoding="utf8") as f:
         data = line.replace("-", "_")
         data = data.split()
         if data[0] != "setName" and len(data) == 4:
-            dataset.append({"unicode": int(data[1]), "name": data[2].replace('_', ' '), "symbol": data[3], "keywords": {}, "symbols": []})
+            dataset.append({"unicode": int(data[1]), "name": data[2].replace('_', ' ').lower(), "symbol": data[3], "keywords": {}, "symbols": []})
 
 with open(sys.argv[1].rsplit('.', 1)[0] + '.json', 'w') as f:
     json.dump(dataset, f, indent=4)
