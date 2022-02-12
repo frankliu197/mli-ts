292c75f841f855b9ba611a3592ef67f78e8b555d
aeiou80 <cameronjdavis@cmail.carleton.ca>
2022-02-12 13:18:11 -0500

    separate symbol files

--- src/Recommender/SymbolSet.ts
+++ src/Recommender/SymbolSet.ts
@@ -1,6 +1,13 @@
 import BasicLatin from "../symbols/BasicLatin.json" 
+import LatinExtended from "../symbols/LatinExtended.json"
+import Greek from "../symbols/Greek.json"
+import MathOperators from "../symbols/MathOperators.json"
+import MiscTechnical from "../symbols/MiscTechnical.json"
+import NumberForms from "../symbols/NumberForms.json"
+import SmallFormVariants from "../symbols/SmallFormVariants.json"
+import SuperscriptsAndSubscripts from "../symbols/SuperscriptsAndSubscripts.json"
 import Character from "./Character"
 
 export default {
-	"BasicLatin": BasicLatin as Character[]
+	"BasicLatin": BasicLatin as unknown as Character[]
 }
