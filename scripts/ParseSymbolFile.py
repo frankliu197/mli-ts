6d3142c201ae807f8486c0f3714a402560440791
aeiou80 <cameronjdavis@cmail.carleton.ca>
2022-02-12 14:47:49 -0500

    re-did the jsons to include more info

--- scripts/ParseSymbolFile.py
+++ scripts/ParseSymbolFile.py
@@ -9,8 +9,8 @@ with open(sys.argv[1], encoding="utf8") as f:
     for line in f:
         data = line.replace("-", "_")
         data = data.split()
-        if data[0] != "setName" and len(data) == 4:
-            dataset.append({"unicode": int(data[1]), "name": data[2].replace('_', ' ').lower(), "symbol": data[3], "keywords": {}, "symbols": []})
+        if len(data) == 4:
+            dataset.append({"unicode": int(data[1]), "name": data[2].replace('_', ' ').lower(), "symbol": data[3], "keywords": [], "strokes": "", "boost": 1})
 
 with open(sys.argv[1].rsplit('.', 1)[0] + '.json', 'w') as f:
     json.dump(dataset, f, indent=4)
