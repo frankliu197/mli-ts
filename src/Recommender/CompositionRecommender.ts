81aa033e43a207a1aee0df6d013b30d64f20858f
frankliu197 <frankliu197@gmail.com>
2022-03-12 23:12:42 -0500

    Fixed composition breakdown of user search terms

--- src/Recommender/CompositionRecommender.ts
+++ src/Recommender/CompositionRecommender.ts
@@ -1,20 +1,35 @@
 import { CompositionTree } from "./CompositionTree";
 import Character from "./Character";
 import SymbolSets from "./SymbolSets";
-
+import { stringSort } from "@/helpers/helpers";
 export class CompositionRecommender {
   tree: CompositionTree;
+  symbolSet: SymbolSets;
+
   constructor() {
     this.tree = new CompositionTree();
   }
   add(set: SymbolSets): void {
+    this.symbolSet = set;
     for (const c of set.symbols) {
       this.tree.insert(c);
     }
   }
 
   suggest(search: string): Map<Character, number> {
-    return this.tree.getCharacterSet(search);
+    //composition breakdown of search terms
+    let s = "";
+    for (const i of search){
+      const c = this.symbolSet.getCharacter(i);
+      if (c){
+        s += c.composition;
+      } else {
+        s += i;
+      }
+    }
+    s = stringSort(s);
+    
+    return this.tree.getCharacterSet(s);
   }
 }
 
