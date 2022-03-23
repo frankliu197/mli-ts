ff972c033e3a0d94445a626b519a876de03a6a14
yjama <ab1239189@gmail.com>
2022-03-23 12:40:45 -0400

    legend changes

--- src/components/Legend.vue
+++ src/components/Legend.vue
@@ -1,138 +1,147 @@
-<template lang =  "pug">   
-    <div class="legend_container">
-        <div>
-            <label class="legend-label">Legend</label>
-        </div>      
-        <div id="legend">          
-            <div id="legend-box"> 
-                <div>  
-                    <div> Next page:</div>
-                    <div id="imagebox"><img src="../assets/up-down.png" alt=""></div>
-                    <div><span>Shift</span> + <span>ArrowDown</span></div>
-                    <a id = 'shiftarrowdown' @mousedown="keep_focus" @click="shift_arrowdown">
-                    </a>
+<template>
+    <Moveable
+        class="Legend"
+
+        ref="moveable on mli web application"
+
+        @drag="movingmethod" 
+        
+        v-bind="moveable" 
+      >
+        <div class="legend_container"> 
+            <div>
+                <label class="legend-label">Legend</label> 
+            </div>      
+            <div id="legend">
+                <div id="legend-box"> 
+                    <div>  
+                        <div> Next page:</div>
+                        <div id="imagebox"><img src="../assets/up-down.png" alt=""></div>
+                        <div><span>Shift</span> + <span>ArrowDown</span></div> 
+                        <a id = 'shiftarrowdown' @mousedown="keep_focus" @click="shift_arrowdown">
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <div id="legend-box" class="border-left">
-                <div>
-                    <div> Prev Page:</div>
-                    <div id="imagebox"><img src="../assets/twoarrow.png" alt=""></div>
-                    <div> <span>Shift</span> + <span>ArrowUp</span></div>
+                <div id="legend-box" class="border-left">
+                    <div>
+                        <div> Prev Page:</div>
+                        <div id="imagebox"><img src="../assets/twoarrow.png" alt=""></div>
+                        <div> <span>Shift</span> + <span>ArrowUp</span></div>
+                    </div> 
+                    <a id = 'shiftarrowup' @click="shift_arrow_up"></a>
                 </div> 
-                <a id = 'shiftarrowup' @click="shift_arrow_up"></a>
-            </div> 
-            <div id="legend-box" class="border-left"> 
-                <div>
-                    <div> Next Entry:</div> 
-                    <div id="imagebox"><img src="../assets/arrowdown32.png" alt=""></div> 
-                    <div> <span>     </span>      <span>ArrowDown</span></div>
+                <div id="legend-box" class="border-left"> 
+                    <div>
+                        <div> Next Entry:</div> 
+                        <div id="imagebox"><img src="../assets/arrowdown32.png" alt=""></div> 
+                        <div> <span>     </span>      <span>ArrowDown</span></div>
+                    </div>
+                    <a id ='down' @mousedown="keep_focus" @click="arrow_down" ></a>
                 </div>
-                <a id ='down' @mousedown="keep_focus" @click="arrow_down" ></a>
+                <div id="legend-box" class="border-left"> 
+                    <div>
+                        <div> Prev Entry:</div>
+                        <div id="imagebox"><img src="../assets/arrowup32.png" alt=""></div>
+                        <div> <span>    </span>   <span>ArrowUp</span></div>
+                    </div>
+                    <a id = 'up' @click="arrow_up"></a>
+                </div>
+                <div id="legend-box" class="border-left">
+                    <div>
+                        <div>Description:</div>
+                        <div id="imagebox"><img src="../assets/tab32.png" alt=""></div>
+                        <div> <span>    </span>   <span>Tab</span></div>
+                    </div>
+                    <a id = 'tab' @click="tab" ></a>
+                </div>
+                <div id="legend-box" class="border-left">
+                    <div>
+                        <div> Enter:</div>
+                        <div id="imagebox"><img src="../assets/enter.png" alt=""></div> 
+                        <div> <span>    </span>   <span>Enter</span></div>
+                    </div>
+                    <a id ='enter' @click="enter"></a>
+                </div>
+                <div id="legend-box" class="border-left"> 
+                    <div>
+                        <div>Toggledropdown:</div>
+                        <div id="imagebox"><img src="../assets/pagedown.png" alt=""></div>
+                        <div> <span>    </span>   <span>PageDown</span></div>
+                    </div>
+                    <a id = 'pagedown' @click="page_down"></a>
+                </div> 
             </div>
-            <div id="legend-box" class="border-left"> 
-                <div>
-                    <div> Prev Entry:</div>
-                    <div id="imagebox"><img src="../assets/arrowup32.png" alt=""></div>
-                    <div> <span>    </span>   <span>ArrowUp</span></div>
-                </div>
-                <a id = 'up' @click="arrow_up"></a>
-            </div>
-            <div id="legend-box" class="border-left">
-                <div>
-                    <div>Description:</div>
-                    <div id="imagebox"><img src="../assets/tab32.png" alt=""></div>
-                    <div> <span>    </span>   <span>Tab</span></div>
-                </div>
-                <a id = 'tab' @click="tab" ></a>
-            </div>
-            <div id="legend-box" class="border-left">
-                <div>
-                    <div> Enter:</div>
-                    <div id="imagebox"><img src="../assets/enter.png" alt=""></div> 
-                    <div> <span>    </span>   <span>Enter</span></div>
-                </div>
-                <a id ='enter' @click="enter"></a>
-            </div>
-            <div id="legend-box" class="border-left"> 
-                <div>
-                    <div>Toggledropdown:</div>
-                    <div id="imagebox"><img src="../assets/pagedown.png" alt=""></div>
-                    <div> <span>    </span>   <span>PageDown</span></div>
-                </div>
-                <a id = 'pagedown' @click="page_down"></a>
-            </div> 
-        </div>
-    </div>
-    
-</template>   
+        </div>  
+      </Moveable>
+</template>
 
+<script>
+import Moveable from "vue-moveable";
 
-<script lang="ts">
-import Vue from "vue"
-export default Vue.extend({
+export default {
   name: "Legend",
-  methods : {
-      keep_focus(e:any){
-          e.preventDefault();
-      },
-      enter(){
-           var element = document.getElementById("suggest_dropdown");
+  components: {
+    Moveable,
+  },
+  data: () => ({
+    moveable: { 
+    draggable: true, 
+    },
+  }),
+  methods: {
+    keep_focus(e){ 
+        e.preventDefault();
+    },
+    enter(){
+        var element = document.getElementById("suggest_dropdown");
 
         element?.dispatchEvent(new KeyboardEvent('keydown', {
             key: 'Enter',
         }));
-
-         
-      },
-      page_down(){
-           var element = document.getElementById("input_box");
+    },
+    page_down(){
+        var element = document.getElementById("input_box");
 
         element?.dispatchEvent(new KeyboardEvent('keydown', {
             key: 'PageDown',
         }));
-
-        
-      },
-      tab(){
-           var element = document.getElementById("suggest_dropdown");
+    },
+    tab(){
+        var element = document.getElementById("suggest_dropdown");
 
         element?.dispatchEvent(new KeyboardEvent('keydown', {
             key: 'Tab',
         }));
-
-       
-      },
-      shift_arrow_up(){
-           var element = document.getElementById("suggest_dropdown");
+    },
+    shift_arrow_up(){
+        var element = document.getElementById("suggest_dropdown");
 
         element?.dispatchEvent(new KeyboardEvent('keydown', {
             key: 'ArrowUp',
             shiftKey: true,
         }));
-
-
-      },
-      arrow_up(){
-           var element = document.getElementById("suggest_dropdown");
+    },
+    arrow_up(){
+        var element = document.getElementById("suggest_dropdown");
 
         element?.dispatchEvent(new KeyboardEvent('keydown', {
             key: 'ArrowUp',
         })); 
-      },
-      arrow_down(event:any){ 
-           
+    },
+    arrow_down(event){ 
+        
         var element = document.getElementById("suggest_dropdown");
 
         element?.dispatchEvent(new KeyboardEvent('keydown', {
             key: 'ArrowDown',
         }));
+        event.preventDefault(); 
+    },
+    movingmethod({ target, transform }) {
+      target.style.transform = transform; 
+    },
 
-        event.preventDefault();
-
-      },
-
-      shift_arrowdown(e:any){
-
+    shift_arrowdown(e){
         e.preventDefault();
 
         var element = document.getElementById("suggest_dropdown");
@@ -144,12 +153,16 @@ export default Vue.extend({
     },
     shift_arrowup(){
         alert("shift_arrowup");
-    }
-}
-});
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped> 
+<style lang="scss">
+    .container{
+        display: table-cell;
+        vertical-align: bottom;
+    }
     #legend{
         display: flex;   
         margin-bottom: 5px;
@@ -192,9 +205,9 @@ export default Vue.extend({
         text-align: center;
     }
 
-    #legend img {
-        width: 32px;
-    }
+    #legend img { 
+        width: 32px; 
+    } 
 
     label.legend-label {
         font-size: 12px;
@@ -204,13 +217,14 @@ export default Vue.extend({
         
     }
 
-    .border-left {
+    .border-left { 
         border-left: 4px solid grey;
     }
-
-         #legend-box>div{
-             padding: 5px;
-         } 
+ 
+    #legend-box>div{
+        padding: 5px; 
+    } 
 
    
-</style> 
+
+</style>     
